938d4994b42b8161aee915f9be3dbe2a5c4e3f57
KalmeMarq <55203647+KalmeMarq@users.noreply.github.com>
2023-09-28 14:49:41 +0100

    Proper key 'bindings'

--- src/main/java/com/mojang/ld22/InputHandler.java
+++ src/main/java/com/mojang/ld22/InputHandler.java
@@ -7,11 +7,22 @@ import java.util.List;
 
 public class InputHandler implements KeyListener {
     public class Key {
+        public int[] keys;
         public int presses, absorbs;
         public boolean down, clicked;
 
-        public Key() {
+        public Key(int... keys) {
+            this.keys = keys;
             InputHandler.this.keys.add(this);
+        }
+
+        public boolean isPressed(int keycode) {
+            for (int key : this.keys) {
+                if (key == keycode) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public void toggle(boolean pressed) {
@@ -33,14 +44,14 @@ public class InputHandler implements KeyListener {
         }
     }
 
-    public List<Key> keys = new ArrayList<Key>();
+    public List<Key> keys = new ArrayList<>();
 
-    public Key up = new Key();
-    public Key down = new Key();
-    public Key left = new Key();
-    public Key right = new Key();
-    public Key attack = new Key();
-    public Key menu = new Key();
+    public Key up = new Key(KeyEvent.VK_NUMPAD8, KeyEvent.VK_W, KeyEvent.VK_UP);
+    public Key down = new Key(KeyEvent.VK_NUMPAD2, KeyEvent.VK_S, KeyEvent.VK_DOWN);
+    public Key left = new Key(KeyEvent.VK_NUMPAD4, KeyEvent.VK_A, KeyEvent.VK_LEFT);
+    public Key right = new Key(KeyEvent.VK_NUMPAD6, KeyEvent.VK_D, KeyEvent.VK_RIGHT);
+    public Key attack = new Key(KeyEvent.VK_SPACE, KeyEvent.VK_CONTROL, KeyEvent.VK_NUMPAD0, KeyEvent.VK_INSERT, KeyEvent.VK_C);
+    public Key menu = new Key(KeyEvent.VK_TAB, KeyEvent.VK_ALT, KeyEvent.VK_ALT_GRAPH, KeyEvent.VK_ENTER, KeyEvent.VK_X);
 
     public void releaseAll() {
         for (Key key : this.keys) {
@@ -67,73 +78,10 @@ public class InputHandler implements KeyListener {
     }
 
     private void toggle(KeyEvent ke, boolean pressed) {
-        if (ke.getKeyCode() == KeyEvent.VK_NUMPAD8) {
-            this.up.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_NUMPAD2) {
-            this.down.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_NUMPAD4) {
-            this.left.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_NUMPAD6) {
-            this.right.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_W) {
-            this.up.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_S) {
-            this.down.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_A) {
-            this.left.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_D) {
-            this.right.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_UP) {
-            this.up.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_DOWN) {
-            this.down.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
-            this.left.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
-            this.right.toggle(pressed);
-        }
-
-        if (ke.getKeyCode() == KeyEvent.VK_TAB) {
-            this.menu.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_ALT) {
-            this.menu.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_ALT_GRAPH) {
-            this.menu.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_SPACE) {
-            this.attack.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_CONTROL) {
-            this.attack.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_NUMPAD0) {
-            this.attack.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_INSERT) {
-            this.attack.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_ENTER) {
-            this.menu.toggle(pressed);
-        }
-
-        if (ke.getKeyCode() == KeyEvent.VK_X) {
-            this.menu.toggle(pressed);
-        }
-        if (ke.getKeyCode() == KeyEvent.VK_C) {
-            this.attack.toggle(pressed);
+        for (Key key : this.keys) {
+            if (key.isPressed(ke.getKeyCode())) {
+                key.toggle(pressed);
+            }
         }
     }
 
